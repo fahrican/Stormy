3c420f1f4904cb4017951a560e19f1cce2414ffd
Fahri MacBook <fahri-can@web.de>
2018-01-26 21:07:27 +0100

    started with network request to forecast.io, error because call on main thread

--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.android.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "210439731034a0102c3f6e4a1f7973f9";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastURL).build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(LOG_TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "IOException caught: ", e);
+        }
     }
 }
