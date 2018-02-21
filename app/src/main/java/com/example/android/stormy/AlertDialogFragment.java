1a9fa3ec9441c6132ec9ff80b9cf259b41d29c4f
Fahri Can <fahri-can@gmx.at>
2018-02-21 22:45:45 +0100

    Add alert dialog for no network connectivity and general error.

--- app/src/main/java/com/example/android/stormy/AlertDialogFragment.java
+++ app/src/main/java/com/example/android/stormy/AlertDialogFragment.java
@@ -8,14 +8,22 @@ import android.os.Bundle;
 
 public class AlertDialogFragment extends DialogFragment {
 
+    //Supply keys for the Bundle
+    public static final String TITLE_ID = "title";
+    public static final String MESSAGE_ID = "message";
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        //Get supplied title and message body.
+        Bundle messages = getArguments();
+
         Context context = getActivity();
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(context.getString(R.string.error_title))
-                .setMessage(context.getString(R.string.error_message))
-                .setPositiveButton(context.getString(R.string.error_button_ok), null);
+
+        builder.setTitle(messages.getString(TITLE_ID));
+        builder.setMessage(messages.getString(MESSAGE_ID));
+        builder.setPositiveButton(context.getString(R.string.error_button_ok), null);
 
         AlertDialog dialog = builder.create();
         return dialog;
--- app/src/main/java/com/example/android/stormy/MainActivity.java
+++ app/src/main/java/com/example/android/stormy/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -24,42 +28,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String apiKey = "210439731034a0102c3f6e4a1f7973f9";
-        double latitude = 9999; //37.8267;
+        double latitude = 37.8267;
         double longitude = -122.4233;
         String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
-        final OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(forecastURL).build();
-        Call call = client.newCall(request);
+        if (isNetworkAvailable()) {
 
-        call.enqueue(new Callback() {
-            @Override public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
+            final OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(forecastURL).build();
+            Call call = client.newCall(request);
 
-            @Override public void onResponse(Call call, Response response) throws IOException {
-
-                try {
-
-                    Log.v(LOG_TAG, response.body().string());
-                    if (response.isSuccessful()) {
-                    }
-                    else {
-                        alertUserAboutError();
-                    }
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "IOException caught: ", e);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+
+                        Log.v(LOG_TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserAboutError(getString(R.string.error_title), getString(R.string.error_message));
+                        }
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "IOException caught: ", e);
+                    }
+                }
+            });
+        }
+        else {
+            alertUserAboutError(getString(R.string.no_network_connection), getString(R.string.no_network_connection_text));
+            Toast.makeText(this, R.string.network_unavailable, Toast.LENGTH_LONG).show();
+        }
 
         Log.d(LOG_TAG, "Main UI code is running!");
     }
 
-    private void alertUserAboutError() {
+    private boolean isNetworkAvailable() {
 
-        AlertDialogFragment dialogFragment = new AlertDialogFragment();
-        dialogFragment.show(getFragmentManager(), "ALERT_DIALOG");
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected()) ? true : false;
+    }
+
+    private void alertUserAboutError(String alertTitle, String alertBody) {
+
+        Bundle messageArgs = new Bundle();
+
+        // Use the arguments to set up the bundle
+        messageArgs.putString(AlertDialogFragment.TITLE_ID, alertTitle);
+        messageArgs.putString(AlertDialogFragment.MESSAGE_ID, alertBody);
+
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.setArguments(messageArgs);
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
