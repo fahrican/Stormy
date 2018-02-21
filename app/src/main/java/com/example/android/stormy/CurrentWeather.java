c86a08c7187a5f77b7261d79e9e540f57974be07
Fahri Can <fahri-can@gmx.at>
2018-02-21 23:42:24 +0100

    Add class for showing weather data.

---
+++ app/src/main/java/com/example/android/stormy/CurrentWeather.java
@@ -0,0 +1,59 @@
+package com.example.android.stormy;
+
+public class CurrentWeather {
+
+    private long time;
+    private String summary;
+    private String icon;
+    private double precipChance;
+    private double temperature;
+    private double humidity;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public double getPrecipChance() {
+        return precipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        this.precipChance = precipChance;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+}
